e509abbf92eb721e565d8d1dbd3d3031831ff64a
MoatazRagabRizk <moataz.muslim.70@gmail.com>
2025-02-14 00:39:29 +0200

    initial commit

---
+++ src/test/java/testPacakage/TestClass.java
@@ -0,0 +1,14 @@
+package testPacakage;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+
+public class TestClass {
+
+    @Test
+    public void test()
+    {
+        WebDriver driver = new ChromeDriver();
+    }
+}
